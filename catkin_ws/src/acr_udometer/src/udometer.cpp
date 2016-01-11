2743b5ca3a3e22db59097053fa53bd87a21088f4
Chiel Bruin <ChielBruin@users.noreply.github.com>
2016-01-11 20:52:33 +0100

    Now gets actual weather data from Buineradar.nl
    Now only prints the data in the console once every 10 seconds.
    This data still needs some processing.

--- catkin_ws/src/acr_udometer/src/udometer.cpp
+++ catkin_ws/src/acr_udometer/src/udometer.cpp
@@ -1,5 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Options.hpp>
 
 #include <sstream>
 
@@ -12,20 +14,23 @@ int main(int argc, char **argv) {
 
 	ros::Publisher udometer_pub = n.advertise<std_msgs::String>("udometer", 5);
 
-	ros::Rate loop_rate(1/60.f);
+	ros::Rate loop_rate(1/10.f);
 
-	while (ros::ok()) {    
-		std_msgs::String msg;
+	while (ros::ok()) {
+		std::ostringstream os;
+		os << curlpp::options::Url("http://gps.buienradar.nl/getrr.php?lat=52&lon=4");	//rain data for Delft
 
-		std::stringstream ss;
-		ss << "hello world ";
-		msg.data = ss.str();
-	
-		ROS_INFO("%s", msg.data.c_str());
-		udometer_pub.publish(msg);
+		ROS_INFO("%s", os.str().c_str());
+		
+		//std_msgs::String msg;
+		//msg.data = ss.str();	
+		//ROS_INFO("%s", msg.data.c_str());
+		//udometer_pub.publish(msg);
 
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
   return 0;
 }
+
+
