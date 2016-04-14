7b671f9c1d41a02a0728c66e85dd039a51d89a14
Chiel Bruin <ChielBruin@users.noreply.github.com>
2016-04-14 19:33:58 +0200

    Added a timeout to the echo
    Prevents the sensor to become stuck when a pulse is missed.

--- catkin_ws/src/acr_hardware/src/ultrasonicSensors/ultrasonicSensors.cpp
+++ catkin_ws/src/acr_hardware/src/ultrasonicSensors/ultrasonicSensors.cpp
@@ -9,14 +9,26 @@
 #define MAX_RANGE 4.0
 #define FIELD_OF_VIEW 0.523598776	// 30 degrees in radians
 
+#define TIMEOUT 100 				// timeout in ms
+
 const int TRIG[] = {1, 2, 3, 4};	// Trigger pins for the sensors
 const int ECHO[] = {5, 6, 7, 8};	// Echo pins for the sensors
 
 using namespace ros;
 
 /**
+ * Check if a timeout happened from a given start time.
+ */
+bool timeout(struct timeval start) {
+	struct timeval now;
+	gettimeofday(&now, NULL);
+	return (now.tv_usec - start.tv_usec) > TIMEOUT * 1000;
+}
+
+/**
  * Measure the distance with the specified sensor.
  * Returns a float containing the measured distance in meters.
+ * Returns -1 when a timout happens;
  */
 float getDistance(int sensor) {
 	digitalWrite(TRIG[sensor], LOW);
@@ -25,11 +37,12 @@ float getDistance(int sensor) {
 	usleep(10);
 	digitalWrite(TRIG[sensor], LOW);
 	
-	struct timeval  tv1, tv2;
-	gettimeofday(&tv1, NULL);
-	 while(digitalRead(ECHO[sensor] == LOW))	// wait for response
-	gettimeofday(&tv2, NULL);
-	float time = (tv2.tv_usec - tv1.tv_usec) / 1000000 + (tv2.tv_sec - tv1.tv_sec);
+	struct timeval  start, end;
+	gettimeofday(&start, NULL);
+	 while(digitalRead(ECHO[sensor] == LOW) && !timeout(start))	// wait for response
+	gettimeofday(&end, NULL);
+	float time = (end.tv_usec - start.tv_usec) / 1000000 + (end.tv_sec - start.tv_sec);
+	if (time > TIMEOUT / 1000.f) return -1;
 	return time * 171.5; // convert to meters
 }
 
@@ -55,12 +68,17 @@ int main(int argc, char **argv) {
 	
 	while(ros::ok()) {
 		for(int i = 0; i < SENSORS; i++) {
+			float dist = getDistance(i);
+			if(dist == -1) {
+				ROS_DEBUG("Sensor %d timed out", i);
+				continue;
+			}
 			sensor_msgs::Range msg;
 			msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
 			msg.min_range = MIN_RANGE;
 			msg.max_range = MAX_RANGE;
 			msg.field_of_view = FIELD_OF_VIEW;
-			msg.range = getDistance(i);
+			msg.range = dist;
 			pub.publish(msg);
 		}
 		spinOnce();		
