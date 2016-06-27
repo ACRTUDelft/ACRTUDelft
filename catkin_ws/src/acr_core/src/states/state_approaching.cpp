1981d668b25d0ad20414c63411d9f6f7afafc6df
osboxes.org <marcel.kuipers@hotmail.com>
2016-06-27 20:37:28 +0100

    working state switching

--- catkin_ws/src/acr_core/src/states/state_approaching.cpp
+++ catkin_ws/src/acr_core/src/states/state_approaching.cpp
@@ -2,6 +2,8 @@
 
 #include "state_interact.cpp"
 
+#define MOVE_SPEED 1
+
 State_Approaching::State_Approaching() { }
 
 State* State_Approaching::update() {
@@ -9,17 +11,14 @@ State* State_Approaching::update() {
 			if(!SensorData::isFree(U_FRONT_TOP)) {
 			return new State_Interact();
 		}
+		float angle = SensorData::pointOfInterest();
+		
+		SensorData::sendTwist(-angle, MOVE_SPEED);
 		return this;
 	}
 	else {
 		return new State_Idle();
 	}
-	/**}
-	if(rand() % 100 < 10) {
-		return new State_LookAround();
-	} else {
-		return this;
-	} **/
 }
 
 void State_Approaching::switchTo() {
